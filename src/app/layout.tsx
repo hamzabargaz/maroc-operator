0d28a8ac9ee29d5b0ce134f686bff9b3dc1ab44c
hamzabargaz <hamza.bargaz@gmail.com>
2023-09-02 19:44:49 +0100

    add open graph, twitter metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import Menu from "@/components/menu";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.marocoperateur.com"),
   title: "Discover Your Phone Operator in Morocco | Maroc Operateur",
   description:
     "Looking for your phone operator in Morocco? Maroc Operateur makes it easy. Find your mobile operator or landline zone with our quick, accurate lookup tool.",
@@ -29,6 +30,32 @@ export const metadata: Metadata = {
       url: "/favicon/apple-touch-icon.png",
     },
   ],
+  openGraph: {
+    title: "Maroc Operateur",
+    description:
+      "Find your mobile operator or landline zone with our quick, accurate lookup tool.",
+    url: "https://www.marocoperateur.com/",
+    siteName: "Next.js",
+    images: [
+      {
+        url: "https://www.marocoperateur.com/og.png",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Maroc Operateur",
+    description:
+      "Find your mobile operator or landline zone with our quick, accurate lookup tool.",
+    siteId: "",
+    creator: "@Hamza_Bargaz",
+    creatorId: "",
+    images: ["https://www.marocoperateur.com/og.png"],
+  },
 };
 
 export default function RootLayout({
--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,4 +1,3 @@
 export { default as IAMlogo } from "./Iam.png";
 export { default as INWIlogo } from "./Inwi.png";
 export { default as ORANGElogo } from "./Orange.png";
-export { default as MarocOperateur } from "./maroc-operateur.png";
