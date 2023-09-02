d4aa83f9890b32efde6722ee7c8e766eac2d79ae
hamzabargaz <hamza.bargaz@gmail.com>
2023-09-02 18:25:44 +0100

    add logo maroc operateur

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,13 +1,17 @@
 "use client";
 import React from "react";
 import { ModeToggle } from "./mode-toggle";
+import { Logo } from "@/assets/icons";
+import Image from "next/image";
 
 type Props = {};
 
 export default function Menu({}: Props) {
   return (
     <header className='flex items-center justify-between w-full mx-auto max-w-4xl mt-10 p-4'>
-      <h1 className='text-2xl font-bold'>Maroc Operator</h1>
+      <div className='w-64 h-20'>
+        <Logo className='object-cover w-full h-full' />
+      </div>
       <nav className='flex items-center justify-between'>
         {/* <a href='#' className='mx-2 text-lg font-semibold'>
           Home
