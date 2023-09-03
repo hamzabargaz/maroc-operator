18aff4607e777616455185e181f33a1e96f0d66a
hamzabargaz <hamza.bargaz@gmail.com>
2023-09-03 01:31:20 +0100

    test fix twitter meta

--- src/app/twitter-image.tsx
+++ src/app/twitter-image.tsx
@@ -1,5 +1,32 @@
+import { ImageResponse } from "next/server";
+
 export const alt = "Maroc Operateur";
 export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
 
-export default function Image() {}
+export default async function Image() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 128,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        Maroc Operateur
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+    }
+  );
+}
